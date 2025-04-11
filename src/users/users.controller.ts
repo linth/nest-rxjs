76ad7b7bd7b3e8b1f083aab821b0fb9a90a852a5
linth <zhua0404@gmail.com>
2025-04-11 14:05:57 +0800

    feat: change user functions.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,41 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { EventBusService } from 'src/event-bus/event-bus.service';
-
-type user = [
-  {id: number, name: string},
-]
-
-const users = [
-  {
-    id: 1,
-    name: 'Alice',
-  },
-  {
-    id: 2,
-    name: 'George',
-  }
-]
 
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly eventBus: EventBusService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Get()
   getAllUser() {
-    const users = [
-      {
-        id: 1,
-        name: 'Alice',
-      },
-      {
-        id: 2,
-        name: 'George',
-      }
-    ]
-
-    this.eventBus.emit('user.getAll', users);
-    return users;
+    return this.usersService.getAllUser();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { EventBusService } from 'src/event-bus/event-bus.service';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(private readonly eventBus: EventBusService) {}
+
+	getAllUser() {
+		const users = [
+      {
+        id: 1,
+        name: 'Alice',
+      },
+      {
+        id: 2,
+        name: 'George',
+      }
+    ]
+
+    this.eventBus.emit('user.getAll', users);
+    return users;
+	}
+
+}
