c440151e0a4a8573bfb69248e1b17b102d461243
linth <georgelin@oringnet.com>
2025-04-11 11:57:33 +0800

    chore: add comments.

--- src/other/other.service.ts
+++ src/other/other.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { SharedService } from 'src/shared/shared.service';
 
+/**
+ * Observer: 負責觀察(訂閱)事件並做出反應 (使用.subscribe())
+ * 
+ * ✅「SharedService 是 observable，OtherService 是 observer（訂閱者）」
+ */
 @Injectable()
 export class OtherService implements OnModuleInit {
 	constructor(
--- src/shared/shared.service.ts
+++ src/shared/shared.service.ts
@@ -3,6 +3,10 @@ import { Subject } from 'rxjs';
 
 /**
  * 在modules內部，建立一個subject提供給外部去訂閱和寫入資料。
+ * 	- 提供一個可重複使用的「事件通道」（比如 RxJS Subject 或 Observable）
+ * 	- 它是 Observable（或包含 Observable），別人會來訂閱它的事件
+ * 
+ * ✅「SharedService 是 observable，OtherService 是 observer（訂閱者）」
  */
 
 @Injectable()
